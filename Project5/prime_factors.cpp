4fa1227376d88d368522fbd2ca79916748276e3f
shoo.song <shoo.song@samsung.com>
2025-03-27 14:29:42 +0900

    enable prime_factors

--- Project5/prime_factors.cpp
+++ Project5/prime_factors.cpp
@@ -1,3 +1,26 @@
-// Ã¹ ¼öÁ¤~~
-// add
-// add
+#include "vector"
+using namespace std;
+class PrimeFactor {
+public:
+	vector<int> of(int number) {
+		vector<int> result = {};
+		int prime_idx = 0;
+		int cur_prime = 0;
+		while (true)
+		{
+			if (number == 1) break;
+			cur_prime = prime_list[prime_idx];
+			if (number % cur_prime == 0) {
+				result.push_back(cur_prime);
+				number /= cur_prime;
+			}
+			else
+			{
+				prime_idx++;
+			}
+		}
+		return result;
+	}
+private:
+	vector<int> prime_list = { 2, 3, 5, 7, 11, 13 };
+};
--- Project5/prime_factors_test.cpp
+++ Project5/prime_factors_test.cpp
@@ -1,4 +1,31 @@
 #include "gmock/gmock.h"
+#include "prime_factors.cpp"
+using namespace std;
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
+TEST(PrimeFactors, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
+TEST(PrimeFactors, Of3) {
+	PrimeFactor prime_factor;
+	vector<int> expected = { 3 };
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
+TEST(PrimeFactors, Of6) {
+	PrimeFactor prime_factor;
+	vector<int> expected = { 2,3 };
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
+TEST(PrimeFactors, Of9) {
+	PrimeFactor prime_factor;
+	vector<int> expected = { 3,3 };
+	EXPECT_EQ(expected, prime_factor.of(9));
+}
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
